c221d8df8afdf9419f6a429c3b3a60555fd847ed
Asraf Uddin Ahmed <13ratul@gmail.com>
2017-09-20 13:27:10 +0600

    Foreground task prepare for short pulling

--- EmptyRoomAlertApp/src/app/app.component.ts
+++ EmptyRoomAlertApp/src/app/app.component.ts
@@ -7,6 +7,10 @@ import { RoomsPage } from '../pages/rooms/rooms';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { BackgroundMode } from '@ionic-native/background-mode';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
+import { RemoteServiceProvider } from '../providers/remote-service/remote-service';
 
 
 @Component({
@@ -23,7 +27,10 @@ export class MyApp {
     public platform: Platform,
     public menu: MenuController,
     public statusBar: StatusBar,
-    public splashScreen: SplashScreen
+    public splashScreen: SplashScreen,
+    private remoteService: RemoteServiceProvider,
+    private backgroundMode: BackgroundMode,
+    private localNotifications: LocalNotifications
   ) {
     this.initializeApp();
 
@@ -38,8 +45,61 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      this.runInBackground();
+      
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+    });
+  }
+
+  private runInBackground() {
+    let intervalInMiliSecond = 30000;
+    let rmtService = this.remoteService;
+    let lclNotifications = this.localNotifications;
+    let fnNotify = this.notifyRoomStateChange;
+
+    this.backgroundMode.enable();
+    this.backgroundMode.on("activate").subscribe(() => {
+      console.log("activating background process");
+      setInterval(function () {
+        console.log("background process");
+        // fnNotify(intervalInMiliSecond, rmtService, lclNotifications);
+      }, intervalInMiliSecond);
+    });
+
+    setInterval(function () {
+      console.log("foreground process");
+      fnNotify(intervalInMiliSecond, rmtService, lclNotifications);
+    }, intervalInMiliSecond);
+    
+  }
+
+  private notifyRoomStateChange(intervalInMiliSecond: number, rmtService: RemoteServiceProvider, lclNotifications: LocalNotifications) {
+    let start = new Date();
+    let end = new Date();
+    end.setMilliseconds(end.getMilliseconds() + intervalInMiliSecond);
+
+    let endPoint = "room-states?searchItem.LogTimeFrom=" + start.toLocaleString()
+      + "&searchItem.LogTimeTo=" + end.toLocaleString()
+      + "&sortBy.fieldName=LogTime&sortBy.isAscending=true&pagination.displayStart=0&pagination.displaySize=100";
+
+    rmtService.get(endPoint).subscribe(data => {
+      let arrRoomStateWithRoom = data.items;
+      console.log(arrRoomStateWithRoom);
+
+      let arrNotification = [];
+      for (let I = 0; I < arrRoomStateWithRoom.length; I++) {
+        arrNotification.push({
+          id: I,
+          title: arrRoomStateWithRoom[I].room.name + ' has been ' + (arrRoomStateWithRoom[I].isEmpty ? 'empty' : 'booked') + ' now',
+          text: arrRoomStateWithRoom[I].room.address,
+          data: arrRoomStateWithRoom[I].room.address
+        });
+      }
+      console.log(arrNotification);
+      lclNotifications.schedule(arrNotification);
+    }, err => {
+      console.log("Oops!");
     });
   }
 
--- EmptyRoomAlertApp/src/pages/home/home.ts
+++ EmptyRoomAlertApp/src/pages/home/home.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import { BackgroundMode } from '@ionic-native/background-mode';
-import { LocalNotifications } from '@ionic-native/local-notifications';
-
-import { RemoteServiceProvider } from '../../providers/remote-service/remote-service';
 import { RoomsPage } from '../rooms/rooms';
 
 @Component({
@@ -12,62 +8,11 @@ import { RoomsPage } from '../rooms/rooms';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(private navCtrl: NavController,
-    private remoteService: RemoteServiceProvider,
-    private backgroundMode: BackgroundMode,
-    private localNotifications: LocalNotifications) {
+  constructor(private navCtrl: NavController) {
 
-    this.runInBackground();
 
   }
 
-  private runInBackground() {
-    let intervalInMiliSecond = 30000;
-    let rmtService = this.remoteService;
-    let lclNotifications = this.localNotifications;
-    let fnNotify = this.notifyRoomStateChange;
-
-    this.backgroundMode.enable();
-    this.backgroundMode.on("activate").subscribe(() => {
-      console.log("activating background process");
-      setInterval(function () {
-        console.log("background process");
-        fnNotify(intervalInMiliSecond, rmtService, lclNotifications);
-      }, intervalInMiliSecond);
-    });
-
-    console.log("foreground process");
-    fnNotify(intervalInMiliSecond, rmtService, lclNotifications);
-  }
-
-  private notifyRoomStateChange(intervalInMiliSecond: number, rmtService: RemoteServiceProvider, lclNotifications: LocalNotifications) {
-    let start = new Date();
-    let end = new Date();
-    end.setMilliseconds(end.getMilliseconds() + intervalInMiliSecond);
-
-    let endPoint = "room-states?searchItem.LogTimeFrom=" + start.toLocaleString()
-      + "&searchItem.LogTimeTo=" + end.toLocaleString()
-      + "&sortBy.fieldName=LogTime&sortBy.isAscending=true&pagination.displayStart=0&pagination.displaySize=100";
-
-    rmtService.get(endPoint).subscribe(data => {
-      let arrRoomStateWithRoom = data.items;
-      console.log(arrRoomStateWithRoom);
-
-      let arrNotification = [];
-      for (let I = 0; I < arrRoomStateWithRoom.length; I++) {
-        arrNotification.push({
-          id: I,
-          title: arrRoomStateWithRoom[I].room.name + ' has been ' + (arrRoomStateWithRoom[I].isEmpty ? 'empty' : 'booked') + ' now',
-          text: arrRoomStateWithRoom[I].room.address,
-          data: arrRoomStateWithRoom[I].room.address
-        });
-      }
-      console.log(arrNotification);
-      lclNotifications.schedule(arrNotification);
-    }, err => {
-      console.log("Oops!");
-    });
-  }
 
   enterRoom(event) {
     this.navCtrl.push(RoomsPage);
