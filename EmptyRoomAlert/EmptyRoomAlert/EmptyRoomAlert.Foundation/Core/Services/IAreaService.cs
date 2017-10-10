0b6a2f3a60fea407b2cb616b9df57533db69f55e
Asraf Uddin Ahmed <13ratul@gmail.com>
2017-10-10 12:11:37 +0600

    AreaService prepare

---
+++ EmptyRoomAlert/EmptyRoomAlert/EmptyRoomAlert.Foundation/Core/Services/IAreaService.cs
@@ -0,0 +1,14 @@
+﻿using EmptyRoomAlert.Foundation.Core.Aggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyRoomAlert.Foundation.Core.Services
+{
+    public interface IAreaService
+    {
+        ICollection<Area> GetAll();
+    }
+}
---
+++ EmptyRoomAlert/EmptyRoomAlert/EmptyRoomAlert.Foundation/Persistence/Services/AreaService.cs
@@ -0,0 +1,31 @@
+﻿using EmptyRoomAlert.Foundation.Core;
+using EmptyRoomAlert.Foundation.Core.Aggregates;
+using EmptyRoomAlert.Foundation.Core.Services;
+using Ninject;
+using Ninject.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyRoomAlert.Foundation.Persistence.Services
+{
+    public class AreaService : IAreaService
+    {
+        private ILogger _logger;
+        private IUnitOfWork _unitOfWork;
+        [Inject]
+        public AreaService(ILogger logger,
+            IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public ICollection<Area> GetAll()
+        {
+            return _unitOfWork.Areas.GetAll();
+        }
+    }
+}
